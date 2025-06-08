d3a97c77fe19cebecb50e6a7713f9f5c7b8e9cc3
dheeraj9307 <dheeraj162001@gmail.com>
2025-06-08 11:44:15 +0530

    Spring MVC Web project

--- sprinbean/src/main/java/com/dheerajpatel/sprinbean/MyBean.java
+++
@@ -1,9 +0,0 @@
-package com.dheerajpatel.sprinbean;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-public class MyBean {
-	public void hello() {
-        System.out.println("Hello from MyBean");
-    }
-}
